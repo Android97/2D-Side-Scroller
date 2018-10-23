fe21f4dcc1d4b09e7e08fe491e4ea195d064ce48
Ho_AndrewL <hoa595@hsd.k12.or.us>
2018-10-23 14:39:31 -0700

    add speed and maxspeed to player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     private Rigidbody m_rb;
+    public float speed = 10.0F;
+    public float max_speed = 12.0F;
+
+    public float Max_speed { get => max_speed; set => max_speed = value; }
 
     // Use this for initialization
     void Start()
@@ -16,5 +20,14 @@ public class PlayerController : MonoBehaviour {
     {
         float movement = Input.GetAxis("Horizontal");
         m_rb.AddForce(new Vector3(movement, 0.0F, 0.0F));
+    }
+
+    private void fixedUpdate()
+    {
+        float movement = Input.GetAxis("Horizonal");
+        m_rb.AddForce(new Vector3(movement * speed, 0.0F, 0.0F));
+        m_rb.velocity = new Vector3(
+            Mathf.Clamp(m_rb.velocity.x, -max_speed, max_speed),
+            m_rb.velocity.y,m_rb.velocity.z);
     }
 }
