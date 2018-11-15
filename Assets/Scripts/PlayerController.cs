b33c41da5d49168bfb1d397852db1a8d420d8136
Android97 <hoa595@hsd.k12.or.us>
2018-11-15 15:32:32 -0800

    Fixed the game and started a GameManager command

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,15 @@ public class PlayerController : MonoBehaviour {
     public float speed = 10.0F;
     public float max_speed = 12.0F;
     public float jump_height = 750.0F;
+    private Collider m_collider;
+    private float collider_radius = 0.0F;
+    public float grounded_epsilon = 0.05F;
+    public int user_layer_platform;
+    private float get_axis_horizontal = 0.0F;
+    private bool get_key_down_space = false;
+    public string pickup_tag;
+    public GameManager gm;
+
     // Use this for initialization
     void Start()
     {
@@ -14,21 +23,40 @@ public class PlayerController : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        float movement = Input.GetAxis("Horizontal");
-        m_rb.AddForce(new Vector3(movement, 0.0F, 0.0F));
+        get_axis_horizontal = Input.GetAxis("Horizontal");
+        get_key_down_space = Input.GetKey(KeyCode.Space);
     }
 
     private void fixedUpdate()
     {
-        float movement = Input.GetAxis("Horizonal");
+        float movement = get_axis_horizontal;
         m_rb.AddForce(new Vector3(movement * speed, 0.0F, 0.0F));
         m_rb.velocity = new Vector3(
             Mathf.Clamp(m_rb.velocity.x, -max_speed, max_speed),
             m_rb.velocity.y,m_rb.velocity.z);
         //Jump Mechanic
-        if (Input.GetKeyDown(KeyCode.Space))
-            m_rb.AddForce(0.0F, Jump_height, 0.0F);
+        if (get_key_down_space && isGrounded())
+            m_rb.AddForce(0.0F, jump_height, 0.0F);
+    }
+
+    bool isGrounded()
+    {
+        int platform_layer = 1 << user_layer_platform;
+        return Physics.Raycast(
+            transform.position,
+            Vector3.down,
+            collider_radius + grounded_epsilon,
+            platform_layer);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag(pickup_tag))
+        {
+            other.gameObject.SetActive(false);
+            gm.score += 50;
+        }
     }
 }
